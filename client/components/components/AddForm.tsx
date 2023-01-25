98adadd3a669f8443be0e4e42f77037ced2f28f5
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-01-25 21:32:35 +0100

    Style add form

--- client/components/components/AddForm.tsx
+++ client/components/components/AddForm.tsx
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import ProfileImage from "./../../public/code-img1.jpg";
 
-const blob = new Blob([]);
-const file = new File([blob], "");
-
 const AddForm = () => {
+  const blob = new Blob(["some-image"], { type: "image/png" });
+  const file = new File([blob], "");
   const [content, setContent] = useState("");
   const [image, setImage] = useState(file);
+
   const userState = useSelector((state) => {
     return state.userState;
   });
@@ -31,51 +31,51 @@ const AddForm = () => {
             className="rounded-full w-12 h-12"
           />
         </div>
-        <form
-          action=""
-          className="flex gap-2 flex-1 bg-gray-600 rounded text-white"
-        >
-          <textarea
-            name=""
-            id=""
-            cols={30}
-            rows={1}
-            className="flex-1 p-2 rounded transparent outline-none bg-transparent resize-y"
-            placeholder={`What's on your mind ${user.firstName}?`}
-            value={content}
-            onChange={(e) => {
-              setContent(e.target.value);
-            }}
-          ></textarea>
-        </form>
-      </div>
-      <div>
-        <div className="">
-          <label
-            htmlFor="postImage"
-            className="w-full h-full cursor-pointer rounded p-2 block text-center bg-gray-900 text-white dark:hover:bg-gray-400 dark:hover:text-black"
-          >
-            Add Image
-          </label>
+        <form action="" className="flex flex-col w-full gap-y-1">
+          <div className="flex gap-2 flex-1 bg-gray-600 rounded text-white gap-x-4">
+            <textarea
+              name=""
+              id=""
+              cols={30}
+              rows={1}
+              className="flex-1 p-2 rounded transparent outline-none bg-transparent resize-y"
+              placeholder={`What's on your mind ${user.firstName}?`}
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+              required
+            ></textarea>
+          </div>
+          <div className="flex gap-x-3">
+            <div className="flex-1 cursor-pointer rounded p-2 text-center bg-gray-900 text-white dark:hover:bg-gray-400 dark:hover:text-black">
+              <label htmlFor="postImage" className="w-full h-full ">
+                Add Image
+              </label>
 
-          <input
-            type="file"
-            name=""
-            id="postImage"
-            className="hidden"
-            accept=".jpg,.webp,.jpeg,.gif,.png"
-            onChange={(e) => {
-              setImage(e.target.files[0]);
-              console.log(e.target.files[0]);
-            }}
-          />
-        </div>
-        <div></div>
-      </div>
-      <div>
-        <button type="submit" onClick={handleSubmit}>
-          Add Post
-        </button>
+              <input
+                type="file"
+                name=""
+                id="postImage"
+                className="hidden"
+                accept=".jpg,.webp,.jpeg,.gif,.png"
+                onChange={(e) => {
+                  setImage(e.target.files[0]);
+                  console.log(e.target.files[0]);
+                }}
+              />
+            </div>
+            <div className="flex-1">
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="bg-gray-900 text-white block w-full h-full"
+              >
+                Add Post
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
