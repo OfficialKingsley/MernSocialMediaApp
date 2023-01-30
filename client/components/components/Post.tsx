636aeabd34b4ec528651d4ea2bce5f229ab56e7f
Kingsley Ihemelandu <kingsleyihemelanduofficial@gmail.com>
2023-01-30 23:04:10 +0100

    Edit style for post

--- client/components/components/Post.tsx
+++ client/components/components/Post.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { IPost } from "../../types/PostInterface";
-
+import { AiOutlineMenu } from "react-icons/ai";
 const Post = ({ post }: { post: IPost }) => {
   return (
     <div className="bg-gray-400 rounded-3xl p-4 dark:bg-gray-900 my-3 dark:text-white">
-      <div className="">
-        <cite className="text-red-600 inline-block mr-4">
-          {post.user && `${post.user.firstName} ${post.user.lastName}`}
-        </cite>
-        <span>{post.updatedAt.toLocaleString()}</span>
+      <div className="flex justify-between">
+        <div>
+          <cite className="text-red-600 inline-block mr-4">
+            {post.user && `${post.user.firstName} ${post.user.lastName}`}
+          </cite>
+          {/* <span>{post.updatedAt.toLocaleString()}</span> */}
+        </div>
+        <div>
+          <AiOutlineMenu />
+        </div>
       </div>
       <p>{post.content}</p>
     </div>
